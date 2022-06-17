60adb724584187a7f122cf8166790d9f7314c5f7
xu756 <756334744@qq.com>
2022-06-17 08:50:19 +0800

    jsmd5

--- src/main.js
+++ src/main.js
@@ -6,7 +6,10 @@ import './plugin/element.js';
 Vue.config.productionTip = false;
 //axios
 import request from './plugin/axios.js';
-Vue.prototype.$http = request;
+Vue.prototype.$http = request;	// 将axios挂载到Vue的原型上，这样所有的组件都可以使用this.$http访问到axios
+// js-md5
+import md5 from 'js-md5';
+Vue.prototype.$md5 = md5;
 new Vue({
 	router,
 	store,
