983356a2f42bc70e43466e9d300c0b57a3414551
xu756 <756334744@qq.com>
2022-06-24 15:27:10 +0800

    tab切换

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -8,8 +8,20 @@
       <div class="form_box">
         <!-- 切换 -->
         <div class="form_tab">
-          <div class="tab_item" @click="tab('login')">登录</div>
-          <div class="tab_item" @click="tab('register')">注册</div>
+          <div
+            class="tab_item"
+            @click="tab('login')"
+            :class="{ active: this.show == 'login' }"
+          >
+            登录
+          </div>
+          <div
+            class="tab_item"
+            @click="tab('register')"
+            :class="{ active: this.show == 'register' }"
+          >
+            注册
+          </div>
         </div>
         <div class="form_container">
           <login-box v-if="show == 'login'"></login-box>
@@ -18,7 +30,7 @@
       </div>
     </div>
   </div>
-</template>
+</template> 
 <script>
 import LoginBox from "./login.vue";
 import RegisterBox from "./register.vue";
@@ -90,8 +102,8 @@ export default {
         width: 100%;
         height: 50px;
         .active {
-          color: #00a0e9;
-          border-bottom: 1px solid #000;
+          color: #00a0e9!important;
+          border-bottom: 1px solid #000 !important;
         }
         .tab_item {
           width: 50%;
@@ -104,13 +116,10 @@ export default {
           font-size: 20px;
           font-weight: bold;
           cursor: pointer;
-          &:hover {
-            color: #00a0e9;
-            border-bottom: 1px solid #000;
-          }
+         
         }
       }
-      .form_container{
+      .form_container {
         margin-top: 20px;
         width: 90%;
         margin-left: 5%;
