d98d2af5f6dec071df57bd1caebbb95700938e27
xu756 <756334744@qq.com>
2022-07-04 14:58:15 +0800

    登录cookies

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,14 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
-
+import VueCookies from 'vue-cookies'
 const routes = [
 	{
 		path: '/',
 		name: 'index',
 		component: () => import('@/pages/index/index.vue')
-	},{
+	},
+	{
 		path: '/login',
 		name: 'login',
 		component: () => import('@/pages/login/index.vue')
@@ -20,5 +21,16 @@ const router = new VueRouter({
 	base: process.env.BASE_URL,
 	routes
 });
+router.beforeEach((to, from, next) => {
+	if (to.path === '/login') {
+		next();
+	} else {
+		if (VueCookies.get('token')) {
+			next();
+		} else {
+			next('/login');
 
+		}
+	}
+});
 export default router;
