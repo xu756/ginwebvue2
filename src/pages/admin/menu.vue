7bf14985f6fa8d68cf3b4244ed9d53d3f013a711
xu756 <756334744@qq.com>
2022-08-10 12:37:15 +0800

    增加路由

---
+++ src/pages/admin/menu.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    面板菜单
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,69 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 Vue.use(VueRouter);
-import VueCookies from 'vue-cookies'
+import VueCookies from "vue-cookies";
 
 const routes = [
-	{
-		path: '/',
-		name: 'index',
-		component: () => import('@/pages/index/index.vue'),
-		redirect: '/home',
-		children: [
-			{
-				path: '/home',
-				name: 'home',
-				component: () => import('@/pages/home/home.vue'),
-			},
-			{
-				path:'/WxchatPublic',
-				name:'WxchatPublic',
-				component: () => import('@/pages/wxchat-public/index.vue'),
-			},
-			{
-				path: '/WxchatPublic/menu',
-				name: 'WxchatPublicMenu',
-				component: () => import('@/pages/wxchat-public/menu.vue'),
-			},{
-				path: '/WxchatPublic/message',
-				name: 'WxchatPublicMessage',
-				component: () => import('@/pages/wxchat-public/message.vue'),
-			}
-		]
-
-	},
-	{
-		path: '/login',
-		name: 'login',
-		component: () => import('@/pages/login/index.vue')
-	}
+  {
+    path: "/",
+    name: "index",
+    component: () => import("@/pages/index/index.vue"),
+    redirect: "/home",
+    children: [
+      {
+        path: "/home",
+        name: "home",
+        component: () => import("@/pages/home/home.vue")
+      },
+      {
+        path: "/WxchatPublic",
+        name: "WxchatPublic",
+        component: () => import("@/pages/wxchat-public/index.vue")
+      },
+      {
+        path: "/WxchatPublic/menu",
+        name: "WxchatPublicMenu",
+        component: () => import("@/pages/wxchat-public/menu.vue")
+      },
+      {
+        path: "/WxchatPublic/message",
+        name: "WxchatPublicMessage",
+        component: () => import("@/pages/wxchat-public/message.vue")
+      },
+      {
+        path: "/admin/menu",
+        name: "adminMenu",
+        component: () => import("@/pages/admin/menu.vue")
+      },
+      {
+        path: "/admin/user",
+        name: "adminUser",
+        component: () => import("@/pages/admin/user.vue")
+      }
+    ]
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/pages/login/index.vue")
+  }
 ];
 
 const router = new VueRouter({
-	mode: 'history',
-	base: process.env.BASE_URL,
-	routes
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
 });
 router.beforeEach((to, from, next) => {
-	
-	if (to.path === '/login') {
-	  next()
-	} else {
-	  if (VueCookies.get('token')) {
-		next()
-	  } else {
-		next('/login')
-	  }
-	}
-  })
+  if (to.path === "/login") {
+    next();
+  } else {
+    if (VueCookies.get("token")) {
+      next();
+    } else {
+      next("/login");
+    }
+  }
+});
 
 export default router;
