26f60bb4be419da8fab7c6b584da0c6766a47182
xu756 <756334744@qq.com>
2022-08-16 00:16:09 +0800

    后端路由

--- server/router/UserRouter.go
+++ server/router/UserRouter.go
@@ -16,6 +16,7 @@ func UserRouter(r *gin.Engine) *gin.Engine {
 	rr.POST("/default", views.Default)                     // 获取默认信息，包括菜单，权限等
 	rr.GET("/get/menu", views.GetMenu)                     //获取菜单数据
 	rr.POST("/update/menus", views.UploadMenu)             // 更新菜单数据
+	rr.GET("/get/articles", views.GetArticle)              // 获取文章数据
 	rr.GET("/getAccessToken", wxchat.GetAccessTokenRouter) // 获取access_token
 	rr.GET("/getmenu", wxchat.GetMenuRouter)               // 获取公众号菜单
 	rr.GET("/getlogs", views.GetLogs)                      // 获取日志
--- server/views/article.go
+++ server/views/article.go
@@ -6,3 +6,50 @@
  */
 
 package views
+
+import (
+	"example.com/mod/models"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func GetArticle(c *gin.Context) {
+
+	models.InitMysqlDB()
+	var db = models.DB
+	var articles []models.Article
+	db.Find(&articles)
+	type Data struct {
+		Page     int `form:"current"`
+		PageSize int `form:"size"`
+	}
+	var data Data
+	c.BindQuery(&data)
+	db.Scopes(Paginate(data.Page, data.PageSize)).Find(&articles)
+	var count int64
+	db.Table("articles").Count(&count)
+	c.JSON(200, gin.H{
+		"type": "success",
+		"msg":  "文章数据获取成功",
+		"data": gin.H{
+			"articles": articles,
+			"total":    count,
+		},
+	})
+}
+
+func Paginate(page int, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if page == 0 {
+			page = 1
+		}
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
+}
